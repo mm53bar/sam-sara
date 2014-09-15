8645174c1f9751fd7bb0894272105f2bc85011c4
Mike McClenaghan <mike@sideline.ca>
2014-09-15 17:42:46 -0600

    Added some mad libs

---
+++ mad_libs/school_lunch.rb
@@ -0,0 +1,20 @@
+container = ask "Pick a type of container"
+adjective = ask "Pick an adjective"
+adjective2 = ask "Pick another adjective"
+noun = ask "Pick a noun"
+animal = ask "Pick an animal"
+vegetable = ask "Pick a vegetable"
+vegetable2 = ask "Pick another vegetable"
+colour = ask "Pick a colour"
+adjective3 = ask "Pick another adjective"
+
+puts "Make sure your lunch " + container +
+    " is filled with nutritious " + adjective +
+    " food. Do not go to the " + adjective2 +
+    " food stand across the street from school. " +
+    " The hamburgers they serve are fried in " + noun +
+    " and are made of " + animal + " meat." +
+    " So take a sandwich made of " + vegetable +
+    " or " + vegetable2 + ". It's much healthier!" +
+    " Drink " + colour + " milk instead of " + adjective3 +
+    " colas."
